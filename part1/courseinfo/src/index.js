39640bab91e345a02963dc9c46cab48b3e397084
Jose <110537455+JBarthaP@users.noreply.github.com>
2022-08-05 12:31:10 +0200

    Exercises  1.3-1.5 done with comments added

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 /*
+Another type of using 
 const Title = ({course}) => <h1>{course}</h1>
 
 */ 
@@ -16,37 +17,70 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  const msgs = props.msgs;
-  const numbers = props.numbers;
+  console.log(props);
+
+  /* If we had ids in the objects we could use this part of the code 
   return (<div>
+  {props.parts.map(item => <Part key= {item.key} msg={item.name} number={item.exercises} />)}
+  </div>)
+  */
+
+  //If you dont have any ids, one possibility is using this:
+  return (
+    <div>
+      {props.parts.map(function(item, i){
+        return <Part msg = {item.name} number = {item.exercises} key={i} />;
+    })}
+    </div>
+  )
+
+  //React uses keys to create child trees to give priority to the objects in an array, I think that how it works but I really dont know :( 
+
+  /* If you dont want to use loops you could always use the safe way
+
+  return (<div>
+    
     <Part msg = {msgs[0]} number = {numbers[0]} ></Part>
     <Part msg = {msgs[1]} number = {numbers[1]} ></Part>
     <Part msg = {msgs[2]} number = {numbers[2]} ></Part>
+    
   </div>)
+
+  */
+  
 }
 
 const Total = (props) => {
   let suma = 0;
-  props.numbers.forEach(element => {
-    suma+= element;
+  props.parts.forEach(element => {
+    suma+= element.exercises;
   });
-  return <p>{props.msg} {suma}</p>
+  return <p>{'Total of exercises '} {suma}</p>
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Title course={course}></Title>
-      <Content msgs= {[part1,part2,part3]} numbers ={[exercises1,exercises2,exercises3]}></Content>
-      <Total msg = {"Number of exercises "} numbers = {[exercises1 , exercises2 , exercises3]}></Total>
+      <Title course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
